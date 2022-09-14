3e0d956f27c4507786d234108f35cc4cbb920c8f
unicorniote <72157764+unicorniote@users.noreply.github.com>
2022-09-14 17:50:24 +0200

    Capa gráfica - Menú

---
+++ LucaSteam/src/gui/Menu.java
@@ -0,0 +1,12 @@
+package gui;
+
+public class Menu {
+	
+	public static void mostrarMenu(){
+		
+		System.out.println("*************LUCASTEAM*************");
+		System.out.println("1. Dar de alta juego");
+		
+	}
+	
+}
