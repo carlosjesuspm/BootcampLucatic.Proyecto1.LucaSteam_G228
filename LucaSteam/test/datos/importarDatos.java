5d82bb946781edb7dc6f4c571f4158efe06bcf28
unicorniote <72157764+unicorniote@users.noreply.github.com>
2022-09-14 23:23:24 +0200

    Test-Paquete datos

---
+++ LucaSteam/test/datos/importarDatos.java
@@ -0,0 +1,20 @@
+package datos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class importarDatos {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testsuma() {
+		int i = 1+2;
+		assertEquals(i, 3);
+	}
+
+}
