45b399962fb6dcc26dffdf9eef5e014b9ca864d9
Lamia <99553152+Lhamia@users.noreply.github.com>
2022-09-15 16:05:28 +0200

    Test-LeerDatos implementado

--- LucaSteam/test/utilidades/TestLeerDatos.java
+++ LucaSteam/test/utilidades/TestLeerDatos.java
@@ -1,9 +1,8 @@
-package servicios;
+package utilidades;
 
 import static org.junit.Assert.*;
 
 import java.util.InputMismatchException;
-
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,32 +14,92 @@ import utilidades.LeerDatos;
 public class TestLeerDatos {
 
 	private static final Logger logger = LogManager.getLogger("ServiciosJuegos");
-	
 
 	@Test
-	public void testDebeRecogerString()throws InputMismatchException {
-		
+	@Ignore
+	public void testDebeRecogerString() throws InputMismatchException {
+
 		String entradaString = LeerDatos.recogerString();
-		String cadenaEsperada="hola";
+		String cadenaEsperada = "hola";
 		logger.info("Test: testDebeRecogerString()");
-		assertEquals(entradaString,cadenaEsperada);
+		assertEquals(entradaString, cadenaEsperada);
 	}
-	
+
 	@Test
 	@Ignore
-	public void testDebeRecogerInt() throws InputMismatchException{
-		int entradaInt = LeerDatos.recogerInt("1");
-		
-		
+	public void testDebeRecogerInt() throws InputMismatchException {
+		int entradaInt = LeerDatos.recogerInt();
+		int intEsperado = 345;
+		assertEquals(entradaInt, intEsperado);
 	}
-	
+
 	@Test
 	@Ignore
-	public void testDebeRecogerFloat()throws InputMismatchException {
-		
-		
+
+	public void testDebeRecogerFloat() throws InputMismatchException {
+
+		float entradaFloat = LeerDatos.recogerFloat();
+		float floatEsperado = 1.45f;
+		assertEquals((Float) entradaFloat, (Float) floatEsperado);
 	}
-	
-	
-	
+
+	@Test
+	@Ignore
+
+	public void testDebeDarErrorSiIntSiendoRecogerString() throws InputMismatchException {
+		String entradaString = LeerDatos.recogerString();
+		int intEsperado = 23;
+		assertEquals(entradaString, intEsperado);
+
+	}
+
+	@Test
+	@Ignore
+
+	public void testDeberiaDarErrorSiFloatSiendoRecogerString() throws InputMismatchException {
+		float entradaFloat = LeerDatos.recogerFloat();
+		String StringEsperado = "hola";
+		assertEquals(StringEsperado, (Float) entradaFloat);
+	}
+
+	@Test
+	@Ignore
+	public void testDeberiaDarErrorSiStringSiendoRecogerInt() throws InputMismatchException {
+		int entradaInt = LeerDatos.recogerInt();
+		String StringEsperado = "hi";
+		assertEquals(entradaInt, StringEsperado);
+	}
+
+	@Test
+	@Ignore
+
+	public void testDeberiaDarErrorSiStringIntRecogerString() throws InputMismatchException {
+		String entradaString = LeerDatos.recogerString();
+		int intEsperado = 43;
+		assertEquals(intEsperado, entradaString);
+	}
+
+	@Test
+	@Ignore
+	public void testExceptionDandoErrorAposta() {
+
+		assertThrows(InputMismatchException.class, () -> {
+			LeerDatos.recogerInt("hi");
+		});
+
+	}
+
+	@Test
+
+	public void testDeberiaErrorSiIntRecogiendoString() throws InputMismatchException {
+		String entradaString = LeerDatos.recogerString();
+		int intEsperado = 56;
+		assertEquals(intEsperado, entradaString);
+	}
+
 }
+	
+	
+	
+
+
