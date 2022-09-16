73ef0d3d78d9ff0e8d7807eb52751ea09edee35e
Alvaro-R <67501790+Alvaro-R@users.noreply.github.com>
2022-09-16 11:29:31 +0200

    generated java-doc

---
+++ LucaSteam/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"control"},{"l":"datos"},{"l":"gui"},{"l":"model"},{"l":"servicios"},{"l":"utilidades"}];updateSearchResults();
